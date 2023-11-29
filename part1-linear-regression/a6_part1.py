c6021e08d980f307067238da577720bdcd6ed79b
Megan Wong <megan.e.wong16@gmail.com>
2023-11-29 11:44:15 -0600

    Write Up

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -6,19 +6,55 @@ from sklearn.linear_model import LinearRegression
 data = pd.read_csv("part1-linear-regression/blood_pressure_data.csv")
 x = data["Age"].values
 y = data["Blood Pressure"].values
+# print(yield)
 
 # Use reshape to turn the x values into 2D arrays:
 x = x.reshape(-1,1)
 
 # Create the model
-
+model=LinearRegression().fit(x,y)
 # Find the coefficient, bias, and r squared values. 
 # Each should be a float and rounded to two decimal places. 
-
+coef = round(float(model.coef_), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(x, y)
+print(coef, intercept, r_squared)
 
 # Print out the linear equation and r squared value
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
+
 
 # Predict the the blood pressure of someone who is 43 years old.
 # Print out the prediction
 
+# value you are going to predict
+x_predict = 43
+# plug that value into your model
+prediction = model.predict([[x_predict]])
+print(x_predict)
+print(f"Prediction when x is {x_predict}: {prediction}")
+
+
 # Create the model in matplotlib and include the line of best fit
+
+
+# sets the size of the graph
+plt.figure(figsize=(6,4))
+
+# creates a scatter plot of originial data in purple
+# and the predicted data in blue
+plt.scatter(x,y, c="purple")
+plt.scatter(x_predict, prediction, c="blue")
+
+# # labe the axes
+# plt.xlabel("Temperature °F")
+# plt.ylabel("Chirps per Minute")
+# plt.title("Cricket Chirps by Temperature")
+
+# plot the line of best fit in red and label the line
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+# show the plot and legend
+plt.legend()
+plt.show()
