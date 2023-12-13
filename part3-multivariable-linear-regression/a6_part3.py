f7adf55f6bd3c944c2b0300afd380666225e2821
Megan Wong <megan.e.wong16@gmail.com>
2023-12-13 11:31:58 -0600

    Write up part 3

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -9,14 +9,43 @@ x = data[["miles","age"]].values
 y = data["Price"].values
 
 #split the data into training and testing data
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
 
 #create linear regression model
+model = LinearRegression().fit(xtrain, ytrain)
 
 #Find and print the coefficients, intercept, and r squared values. 
 #Each should be rounded to two decimal places. 
+coef = np.around(model.coef_, 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = round(model.score(x, y),2)
 
+
+#get the predicted values
+predict = model.predict(xtest)
+predict = np.around(predict, 2)
+
+# print out the linear equation and r^2 value
+print(f"Model's Linear Equation: y={coef[0]}x1 + {coef[1]}x2 + {intercept}")
+print("R Squared value:", r_squared)
 
 #Loop through the data and print out the predicted prices and the 
 #actual prices
+for index in range(len(xtest)):
+    actual = ytest[index] # gets the actual y value from the ytest dataset
+    predicted_y = predict[index] # gets the predicted y value from the predict variable
+    x_coord = xtest[index] # gets the x value from the xtest dataset
+    x_coord = np.around(x_coord, 2)
+
+    print(f"Miles: {x_coord[0]} Age: {x_coord[1]} Actual: {actual} Predicted: {predicted_y}")
+    # print(f"Miles: {150000} Car Age: {20} Actual price: {actual} Predicted price: {predicted_y}")
+
+my_cars = [[89,10], [150,20]]
+my_predictions = np.around(model.predict(my_cars), 2)
+print(my_predictions)
+
+
 print("***************")
 print("Testing Results")
+
+
